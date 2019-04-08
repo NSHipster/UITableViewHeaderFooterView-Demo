9f4bbdd89f4a4d3eb213f8e39da9aa4486cd5132
Mattt <mattt@me.com>
2019-04-08 04:41:55 -0700

    Initial Commit

---
+++ Dairy Stats/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dairy Stats
+//
+//  Created by Mattt Zmuda on 4/8/19.
+//  Copyright © 2019 NSHipster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
